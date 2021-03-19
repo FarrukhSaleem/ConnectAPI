7b732e5d3e525e9356ac5949a55f35f69d604174
Farrukh Saleem Sheikh <farrukhsaleem_01@yahoo.com>
2021-03-19 20:02:01 +0500

    Get & POST

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -7,17 +7,26 @@ import { Http } from '@angular/http';
   styleUrls: ['./posts.component.css']
 })
 export class PostsComponent implements OnInit {
-posts: any[];
-  constructor(http: Http) { 
+  posts: any[];
+  private baseURL: string = "https://localhost:44369/";
+  constructor(private http: Http) {
 
-http.get('http://jsonplaceholder.typicode.com/posts')
-.subscribe(response =>{
-  this.posts=response.json();
-  });
-
+    this.http.get(this.baseURL+"api/post/GetAllPosts")
+      .subscribe(response => {
+        this.posts = response.json();
+      });
   }
-
   ngOnInit(): void {
   }
+  CreatePost(inputControl: HTMLInputElement) {
+    let post:any = { title: inputControl.value }
+    //let post = { title: inputControl.value }
 
+    this.http.post(this.baseURL, JSON.stringify(post))
+      .subscribe(response => {
+        post.id = response.json();
+        //post['id']=response.json();
+        this.posts.splice(0,0,post);
+      });
+  }
 }
